0c9485365619ee3213a3b46dd666cf1668a301dc
vaibhavi saple <tai858294@gmail.com>
2025-09-27 15:24:26 +0530

    python flask change 2

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -3,7 +3,7 @@
 cd /home/ubuntu
 yes | sudo apt update
 yes | sudo apt install python3 python3-pip
-git clone https://github.com/rahulwagh/python-mysql-db-proj-1.git
+git clone https://github.com/vaibhsaple/python-mysql-db-proj-1.git
 sleep 20
 # shellcheck disable=SC2164
 cd python-mysql-db-proj-1
